2f488d113998ead7622537cb5deb2003b9289fd0
tdavis24 <144742362+tdavis24@users.noreply.github.com>
2024-12-03 14:10:44 -0600

    Worked on CLI class and Database class

--- src/Database/Routine Helper App.sql
+++ src/Database/Routine Helper App.sql
@@ -12,7 +12,13 @@ create table UserAccounts (
 create table Routine (
     routineID NUMBER PRIMARY KEY,
     name VARCHAR2(50) NOT NULL,
-    category VARCHAR2(100) NOT NULL,
     recurrance VARCHAR2(100) NOT NULL,
-    FOREIGN KEY (username) REFERENCES UserACcounts(username)
+    FOREIGN KEY (category) REFERENCES Category(name),
+    FOREIGN KEY (username) REFERENCES UserAccounts(username)
+);
+
+create table Category (
+    name VARCHAR2(100) PRIMARY KEY,
+    type VARCHAR2(100) NOT NULL,
+    FOREIGN KEY (username) REFERENCES UserAccounts(username)
 );
