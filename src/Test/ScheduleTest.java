778ab8655277f7e3ad753bb35ca811d14f9dcce0
MichaelRosenwinkel2003 <merosenwinkel@gmail.com>
2024-12-08 22:01:40 -0600

    Starting test cases

--- src/Test/ScheduleTest.java
+++ src/Test/ScheduleTest.java
@@ -14,6 +14,19 @@ public class ScheduleTest {
     @Before
     public void setup(){
         Category healthCategory = new Category("health", "idk");
-        
+        Task t1 = new Task("brush teeth", "brush teeth for 2 minutes", "deadline", LocalTime.of(10, 30), LocalDate.of(2024, 12, 9), "Daily", LocalTime.of(1,0), healthCategory);
+        Schedule mySched = new Schedule(1);
+        mySched.addTask(t1);
+        String scheduleString = mySched.display();
+
+
+    }
+
+    @Test
+    public void testSingularTaskDaily()
+    {
+        Assert.assertEquals(scheduleString, "Name: brush teeth\nInformation: brush teeth for 2 minutes\n" + 
+        "Deadline: deadline\n" +
+        "Time of Day: 10:30\n");
     }
 }
